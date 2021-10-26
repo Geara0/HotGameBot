24cbee7789f320842bc49e744876f0ccf85e7c6d
Geara0 <adrezz.26.5@gmail.com>
2021-10-26 08:03:35 +0500

    Реализация через Set, добавлен таймер

--- HotGameBot/src/main/java/Main.java
+++ HotGameBot/src/main/java/Main.java
@@ -1,32 +1,53 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+import java.util.function.Consumer;
 
 public class Main {
-    private HashMap<String, Title> Titles = new HashMap<>();
-    private static String jsonPath = "";
+    private static String titlesPath = "JSONs/AllTitles";
+    private static String usersPath = "JSONs/HWUserSubsList";
     private static HashSet<Title> allTitles = new HashSet<>();
+    private static HashSet<User> userList;
+    private static HashMap<Title, Game> gameSet;
+    private static Set<Title> titleSet;
+
+    public Main() {
+        try {
+            //userList = new ObjectMapper().readValue(usersPath, HashSet.class);
+            //titleSet = new GenericParser<Set<Title>>.parse(titlesPath);
+            userList = new Gson().fromJson(Files.readString(Path.of(usersPath)), new TypeToken<HashSet<User>>(){}.getType());
+            titleSet = new Gson().fromJson(Files.readString(Path.of(titlesPath)), new TypeToken<Set<Title>>(){}.getType());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        gameSet = parseUsers(userList);
+        TimerTask timerTask = new CheckGamesUpdTimer(titlesPath, titleSet, gameSet);
+        // стартуем TimerTask в виде демона
+
+        Timer timer = new Timer(true);
+        // будем запускать каждых 10 секунд (10 * 1000 миллисекунд)
+        timer.scheduleAtFixedRate(timerTask, 10 * 1000, 10 * 1000);
+    }
 
     /**
      * Функция, отправляющая уведомление
      *
-     * @param name    тайтл
-     * @param link    ссылка на HotGame
-     * @param buyLink ссылка на предложение
-     * @param price   цена
+     * @param game игра, для которой нужно вывести уведомление
      * @return уведомление
      */
-    public String sendNotification(String name, String link, String buyLink, int price) {
-        if (!Titles.containsKey(link)) {
-            Titles.put(link, new Title(name, link, buyLink, price));
-            return null;
-        }
+    public static String createNotification(Game game, Title titleUpd) {
         String notification = null;
-        Title title = Titles.get(link);
-        if (price < title.Price) {
-            title.BuyLink = buyLink;
+        Title title = game.title;
+        if (titleUpd.Price < title.Price) {
             notification = String.format("New best price for %s:\n Prev: %s,\nCurrent: %s\nHotGame link: %s,\nBuy link: %s",
-                    title.getName(), title.Price, price, title.getLink(), title.BuyLink);
+                    title.getName(), title.Price, titleUpd.Price, title.getLink(), titleUpd.BuyLink);
         }
-        title.Price = price;
         return notification;
     }
 
@@ -36,8 +57,8 @@ public class Main {
      * @param users пользователи
      * @return множество игр
      */
-    public static HashSet<Game> parseUsers(HashSet<User> users) {
-        HashSet<Game> gameSet = new HashSet<>();
+    public static HashMap<Title, Game> parseUsers(Iterable<User> users) {
+        HashMap<Title, Game> gameSet = new HashMap<>();
         HashMap<Title, HashSet<User>> gameList = new HashMap<>();
         for (var user : users) {
             for (var title : user.getTitles().values()) {
@@ -49,7 +70,7 @@ public class Main {
         }
 
         for (var game : gameList.entrySet())
-            gameSet.add(new Game(game.getKey(), game.getValue()));
+            gameSet.put(game.getKey(), new Game(game.getKey(), game.getValue()));
         return gameSet;
     }
 
@@ -67,33 +88,5 @@ public class Main {
         }
         return userList;
     }
-
 }
-/* А надо ли это вообще?
-    public static class ParseTimer extends TimerTask {
-    public static void Main() {
-        TimerTask checkTimer = new ParseTimer();
-        // стартуем TimerTask в виде демона
-
-        Timer timer = new Timer(true);
-        // будем запускать каждых 10 секунд (10 * 1000 миллисекунд)
-        timer.scheduleAtFixedRate(checkTimer, 0, 10 * 1000);
-        System.out.println("TimerTask начал выполнение");
-    }
-
-        @Override
-        public void run() {
-            var userList = parseUserData(parseJson(jsonPath));
-            for (var user:userList) {
-               if (users.contains(user)){
-                   for (var title :
-                           user.getTitles()) {
-
-                   }
-               }
-            }
-        }
-    }
-
- */
 
