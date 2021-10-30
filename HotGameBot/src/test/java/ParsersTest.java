4340f4020922f0a825cd1b5c50f7ffce10380d86
Вадим <bigwhiterussianbatya@gmail.com>
2021-10-31 01:56:37 +0500

    Написал тест для метода writeUserSubs() из Main, проверяет корректно ли записывает и потом читает, тест проходит = не нужен

--- HotGameBot/src/test/java/ParsersTest.java
+++ HotGameBot/src/test/java/ParsersTest.java
@@ -1,10 +1,12 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 /**
  * Тесты для parseUsers & parseUserData
@@ -14,10 +16,23 @@ import java.util.Map;
  * @see Main()
  */
 public class ParsersTest {
+    private static final String usersPath = ".\\JSONs\\HWUserSubsList";
+    private static final String usersPathTest = ".\\JSONs\\Users";
     @Test
     public void parseUsers() throws InterruptedException {
         var a = 0;
         new Main();
         Thread.sleep( 10 * 1000);
+    }
+
+    @Test
+    public void writingParsingTest() throws IOException {
+        HashMap<String,User> userMap = new Gson().fromJson(Files.readString(Path.of(usersPath)), new TypeToken<HashMap<String, User>>() {}.getType());
+        Main.writeUserSubs(userMap);
+        HashMap<String,User> newUserMap = new Gson().fromJson(Files.readString(Path.of(usersPathTest)), new TypeToken<HashMap<String, User>>() {}.getType());
+        boolean flag = true;
+        for(var username : userMap.keySet())
+            flag = userMap.get(username).equals(newUserMap.get(username));
+        Assertions.assertTrue(flag);
     }
 }
