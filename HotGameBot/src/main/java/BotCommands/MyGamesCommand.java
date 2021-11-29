26c89d4f02cc9a1294298127954de9865c20e967
Geara0 <adrezz.26.5@gmail.com>
2021-11-30 04:38:50 +0500

    mygames command

---
+++ HotGameBot/src/main/java/BotCommands/MyGamesCommand.java
@@ -0,0 +1,31 @@
+package BotCommands;
+
+import Entities.Title;
+import db.dbWorker;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+public class MyGamesCommand extends Command {
+    public MyGamesCommand() {
+        super("myGames", "my subscriptions list");
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+        var message = new SendMessage();
+        message.setText(dbWorker.getSubscriptions(user.getUserName()));
+        execute(absSender, message, user);
+    }
+
+    private String getUserSubsMessage(Entities.User user) {
+        var subs = new StringBuilder();
+        if (user.getTitles().size() == 0)
+            return CommandsConstants.NO_SUBS.toStringValue();
+        int i = 1;
+        for (Title title : user.getTitles().values())
+            subs.append(String.format("#%d ---- ", i++)).append(title.getStringForm());
+        return subs.toString();
+    }
+}
