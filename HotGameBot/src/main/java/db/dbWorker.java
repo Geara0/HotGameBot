fe20e23641e74193abf9be1a1b675f939a2c42f0
Geara0 <adrezz.26.5@gmail.com>
2021-11-30 10:55:01 +0500

    User identification by ID

--- HotGameBot/src/main/java/db/dbWorker.java
+++ HotGameBot/src/main/java/db/dbWorker.java
@@ -2,7 +2,17 @@ package db;
 
 public class dbWorker {
     //TODO: Получить подписки с бд
-    public static String getSubscriptions(String username) {
+    public static String getSubscriptions(long userId) {
         return "not implemented";
+    }
+
+    //TODO: Подписать пользователя
+    public static boolean subscribeUser(long userId, String title) {
+        return false;
+    }
+
+    //TODO: Отписать пользователя
+    public static boolean unsubscribeUser(long userId, String title) {
+        return false;
     }
 }
