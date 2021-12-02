691c0ca7a48faef2e1e53c7bbbae55fd06965432
Вадим <bigwhiterussianbatya@gmail.com>
2021-12-02 15:38:12 +0500

    добавил парсинг по имени и тест для проверки длительности работы, пока не устраивает(3-4 секунды)

--- HotGameBot/src/main/java/HotGameParsing/HotGameParser.java
+++ HotGameBot/src/main/java/HotGameParsing/HotGameParser.java
@@ -4,11 +4,10 @@ import Entities.Title;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.springframework.format.datetime.joda.DateTimeParser;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -16,36 +15,28 @@ import java.util.GregorianCalendar;
 public class HotGameParser implements Parser {
     @Override
     public Title parseTitle(String link) {
-
-        return null;
+        return getTitleInfoSelect(link);
     }
 
-    public String getJson(){
-        return "";
-    }
-
-    public String getTitleInfo(String link){
-        String result="";
+    public ArrayList<Title> getTitlesByName(String name) {
+        ArrayList<Title> result = new ArrayList<>();
+        String searchName = name.strip().toLowerCase();
+        String searchUrl = "https://hot-game.info/q=".concat(searchName);
         try {
-            Document doc = Jsoup.connect(link).get();
-            Element gameInfo = doc.getElementsByAttributeValue("class", "game-info").first();
-            String titleName = gameInfo.getElementsByAttributeValue("itemprop","name").text();
-            String[] redText = gameInfo.getElementsByAttributeValue("class","red").eachText().toArray(new String[]{});
-            var publisher = redText[redText.length-1];
-            var developer = redText[redText.length-2];
-            var platforms = redText[0].split(",");
-            var releaseDate = gameInfo.getElementsByAttributeValue("class","gray").text();
-            var genres = gameInfo.getElementsByAttributeValueStarting("class","hidden-link genre").text().split(" ");
-            var mode = gameInfo.getElementsByAttribute("title").first().attributes().get("title");
-            var s = gameInfo.children();
-            result = "";
+            Document doc = Jsoup.connect(searchUrl).get();
+            Element searchResults = doc.selectFirst("body > div.container.content-container > section.yui3-cssreset.result-block.content-table");
+            int childrenCount = searchResults.children().size();
+            for (var i=0;i<childrenCount;i++) {
+                var href = searchResults.child(i).selectFirst("a").attr("href");
+                result.add(getTitleInfoSelect("https://hot-game.info".concat(href)));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
 
-    public Title getTitleInfoSelect(String link){
+    public Title getTitleInfoSelect(String link) {
         Title result;
         try {
             Document doc = Jsoup.connect(link).get();
@@ -55,14 +46,14 @@ public class HotGameParser implements Parser {
             var publisher = gameInfo.selectFirst("div.game-publisher > span").text();
             var platforms = gameInfo.selectFirst("div.game-title > span.red").text().split(",");
             var releaseDate = gameInfo.selectFirst("div.game-release-date > span").text();
-            var genres = gameInfo.selectFirst("div.game-genres").getElementsByAttributeValueStarting("class","hidden-link genre").text().split(" ");
+            var genres = gameInfo.selectFirst("div.game-genres").getElementsByAttributeValueStarting("class", "hidden-link genre").text().split(" ");
             var mode = gameInfo.selectFirst("div.game-genres > div > span:nth-child(1)").attributes().get("title");
             var bestMarket = doc.selectFirst("#prices_block > div.game-prices-wrap > div.game-prices-list.game-prices-new > div:nth-child(1)");
             var bestPrice = bestMarket.child(1).selectFirst("div.price-col-3 > div.game-price > span").text();
             var bestLink = bestMarket.child(0).attributes().get("data-href");
             var date = parseDate(releaseDate);
             var isMultiplayer = isMultiplayer(mode);
-            result = new Title(name,link,bestLink,Integer.parseInt(bestPrice),publisher,developer,date,genres,isMultiplayer);
+            result = new Title(name, link, bestLink, Integer.parseInt(bestPrice), publisher, developer, date, genres, isMultiplayer);
         } catch (IOException e) {
             System.out.println("error 404");
             result = new Title();
@@ -70,11 +61,11 @@ public class HotGameParser implements Parser {
         return result;
     }
 
-    private Date parseDate(String date){
+    private Date parseDate(String date) {
         var splitDate = date.split(" ");
         var day = Integer.parseInt(splitDate[0]);
         var year = Integer.parseInt(splitDate[2].split("г")[0]);
-        var month = switch (splitDate[1]){
+        var month = switch (splitDate[1]) {
             case "янв." -> Calendar.JANUARY;
             case "февр." -> Calendar.FEBRUARY;
             case "март" -> Calendar.MARCH;
@@ -89,10 +80,10 @@ public class HotGameParser implements Parser {
             case "дек." -> Calendar.DECEMBER;
             default -> Calendar.UNDECIMBER;
         };
-        return new GregorianCalendar(year,month,day).getTime();
+        return new GregorianCalendar(year, month, day).getTime();
     }
 
-    private boolean isMultiplayer(String mode){
+    private boolean isMultiplayer(String mode) {
         return !"Режим игры: singleplayer".equals(mode);
     }
 }
--- HotGameBot/src/test/java/HotGameParsingTests/HotGameParserTest.java
+++ HotGameBot/src/test/java/HotGameParsingTests/HotGameParserTest.java
@@ -1,8 +1,6 @@
 package HotGameParsingTests;
 
 import HotGameParsing.HotGameParser;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HotGameParserTest {
@@ -11,31 +9,12 @@ public class HotGameParserTest {
 
     @Test
     public void warmUp(){
-        var result = parser.getTitleInfo(link);
         var r2 = parser.getTitleInfoSelect(link);
-    }
-
-    @Test
-    public void getSomething() {
-        parser.getTitleInfo(link);
     }
 
     @Test
     public void getInfoSelect(){
         parser.getTitleInfoSelect(link);
-    }
-
-    @Test
-    public void parse100Times() {
-        for (int i = 0; i < 100; i++) {
-            parser.getTitleInfo(link);
-        }
-    }
-
-    @Test
-    public void parse100TimesAgain() {
-        for (int i = 0; i < 100; i++)
-            parser.getTitleInfo(link);
     }
 
     @Test
@@ -48,5 +27,10 @@ public class HotGameParserTest {
     public void parse100TimeSelectAgain(){
         for (int i = 0; i < 100; i++)
             parser.getTitleInfoSelect(link);
+    }
+
+    @Test
+    public void getInfoByNameTest(){
+        var title = parser.getTitlesByName("a");
     }
 }
