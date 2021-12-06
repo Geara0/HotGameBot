cccf59ac6df6d5e09f2d2d1474fda5d90f7c5ee9
Geara0 <adrezz.26.5@gmail.com>
2021-12-07 00:46:55 +0500

    DBConstants

---
+++ HotGameBot/src/main/java/db/DBConstants.java
@@ -0,0 +1,22 @@
+package db;
+
+public enum DBConstants {
+    NAME_MAX(100),
+    LINK_MAX(150),
+    BUY_LINK_MAX(400),
+    PRICE_MAX(10),
+    DEVELOPER_MAX(200),
+    PUBLISHER_MAX(200),
+    GENRE_MAX(20),
+    DESCRIPTION_MAX(3000);
+
+    private final int value;
+
+    DBConstants(int value) {
+        this.value = value;
+    }
+
+    public int toIntValue() {
+        return value;
+    }
+}
