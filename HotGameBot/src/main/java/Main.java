e15cde9cad3c2a4301b0057f8a9396ec9394cd8b
Geara0 <adrezz.26.5@gmail.com>
2021-12-07 00:42:28 +0500

    idk

--- HotGameBot/src/main/java/Main.java
+++ HotGameBot/src/main/java/Main.java
@@ -4,28 +4,46 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        startBot();
+        //startBot();
+        db();
 
 
-        Scanner scanner = new Scanner(System.in);
-        ConsoleGamesService bot = new ConsoleGamesService();
-        bot.start();
-        while (bot.isRunning()) {
-            User currentUser = bot.getUser();
-            while (bot.hasUser()) {
-                System.out.println("Введите комманду:");
-                String message = scanner.nextLine();
-                bot.runCommand(message, currentUser);
-            }
+        //Scanner scanner = new Scanner(System.in);
+        //ConsoleGamesService bot = new ConsoleGamesService();
+        //bot.start();
+        //while (bot.isRunning()) {
+        //    User currentUser = bot.getUser();
+        //    while (bot.hasUser()) {
+        //        System.out.println("Введите комманду:");
+        //        String message = scanner.nextLine();
+        //        bot.runCommand(message, currentUser);
+        //    }
+        //}
+    }
+
+    private static void db() {
+        var jdbcURL = "jdbc:postgresql://localhost:5432/postgres";
+        var userName = "postgres";
+        var password = "ChPs2345";
+        try {
+            var connection = DriverManager.getConnection(jdbcURL, userName, password);
+            String sql = "INSERT INTO users (id, subscriptions)" +
+                    "VALUES ('12', '\"1\"=>null, \"2\"=>null');";
+            var statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    private static void startBot(){
+    private static void startBot() {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new HelloBot());
