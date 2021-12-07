5ac889166382e3fe9d5961c8884cddcbf2344809
Geara0 <adrezz.26.5@gmail.com>
2021-12-07 05:55:06 +0500

    getSubs

--- HotGameBot/src/main/java/db/DBWorker.java
+++ HotGameBot/src/main/java/db/DBWorker.java
@@ -1,22 +1,57 @@
 package db;
 
-public class DBWorker {
-    //TODO: Получить подписки с бд
-    public static String getSubscriptions(long userId) {
-        return "not implemented";
+import entities.Title;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Set;
+
+import static db.Converter.*;
+
+public class DBWorker implements IDB {
+    private final String jdbcURL = "jdbc:postgresql://localhost:5432/postgres";
+    private final String userName = "postgres";
+    private final String password = "ChPs2345";
+    private Connection connection;
+
+    public DBWorker() {
+        try {
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    //TODO: Подписать пользователя
-    public static boolean subscribeUser(long userId, String title) {
-        return false;
+    @Override
+    public String[] getSubscriptions(long userId) {
+        String[] subscriptions = null;
+        var result = ExecuteSQL(connection, String.format(
+                "SELECT subscriptions FROM users WHERE (id == %s)", userId));
+        var subscriptionsIds = HstoreToSet(result, Integer.class);
+        result = ExecuteSQL(connection, String.format(
+                "SELECT title FROM games WHERE (id IN %s)", subscriptionsIds));
+        try {
+            subscriptions = ArrayToSet(result.getArray("title"), String.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return subscriptions;
     }
 
-    public static boolean unsubscribeAllUser(long userId) {
-        return false;
+    @Override
+    public ReportState subscribeUser(long userId, String title) {
+        return null;
     }
 
-    //TODO: Отписать пользователя
-    public static boolean unsubscribeUser(long userId, String title) {
-        return false;
+    @Override
+    public ReportState unsubscribeAllUser(long userId) {
+        return null;
+    }
+
+    @Override
+    public ReportState unsubscribeUser(long userId, String title) {
+        return null;
     }
 }
